4339970b826843a99783c7ec6a768ddf35823854
gwg <gwg@gwgs-MacBook-Pro.local>
2020-06-06 21:54:21 -0400

    working but sloppy version of router ip list

--- devices/router.py
+++ devices/router.py
@@ -1,4 +1,9 @@
-from urllib import request
+import re
+import json
+import requests
+import hashlib
+import base64
+import urllib.parse
 
 import appsecrets
 
@@ -12,8 +17,11 @@ class TPLink(object):
             self.secrets = appsecrets.CachedSecrets()
         self.ip = router_ip
         self.auth_initialized = False
+        self.auth = None
+        self.session = requests.Session()
+        self.session_url_path_prefix = None
 
-    def setup_auth(self):
+    def setup_auth_OLD(self):
         if self.auth_initialized:
             return
         username = self.secrets.get_password('router.username')
@@ -23,20 +31,75 @@ class TPLink(object):
         request.install_opener(request.build_opener(request.HTTPBasicAuthHandler(password_manager)))
         self.auth_initialized = True
 
+    def get_auth(self):
+        if not self.auth:
+            secrets = appsecrets.CachedSecrets()
+            username = secrets.get_password('router.username')
+            password = secrets.get_password('router.password')
+            self.auth = (username, password)
+        return self.auth
+
+    def make_fake_cookie(self):
+        username, password = self.get_auth()
+        password_hash = hashlib.md5(password.encode('utf8')).hexdigest()
+        authorization_string = username+':'+password_hash
+        authorization_b64 = base64.b64encode(authorization_string.encode('utf8'))
+        authorization_cookie_string = 'Basic '+authorization_b64.decode('utf8')
+        cookie_string = 'Authorization='+urllib.parse.quote(authorization_cookie_string)+';path=/'
+        return cookie_string
+
+    def login(self):
+        headers = {
+                'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36',
+                'Accept': '*/*',
+        }
+        self.session.headers.update(headers)
+        self.session.headers.update({'Cookie':'Authorization='})
+        _ = self.session.get(self.url_base())
+        self.session.headers.update({'Cookie':self.make_fake_cookie()})
+        url = self.url_base()+'/userRpm/LoginRpm.htm?Save=Save'
+        result = self.session.get(url)
+        result_text = result.text
+        matcher = re.compile('location\\.href = "'+self.url_base()+'/([A-Z]*)/.*\\.htm"')
+        url_piece = re.search(matcher, result_text).groups()[0]
+        self.session_url_path_prefix = url_piece
+        return True
+
     def url_base(self):
         return 'http://'+self.ip
 
+    def build_headers(self):
+        return {
+                'Referer':self.url_base()+'/userRpm/MenuRpm.htm'
+                }
+
     def get_path(self, path):
-        self.setup_auth()
-        url = self.url_base()+path
-        content = request.urlopen(url).read()
+        url = '/'.join([self.url_base(), self.session_url_path_prefix, path])
+        content = self.session.get(url).text
         return content
 
     def get_client_ips(self):
-        content = self.get_path('/userRpm/AssignedIpAddrListRpm.htm')
-        print(content)
-        import pdb; pdb.set_trace()
+        self.login()
+        content = self.get_path('userRpm/AssignedIpAddrListRpm.htm')
+        content_no_newlines = content.replace('\n', '').replace('\r', '')
+        first_script, _, _ = content_no_newlines.partition('</SCRIPT>')
+        if not 'DHCPDynList' in first_script:
+            raise RuntimeError('Failed to parse client list.')
+        _, _, approx_array = first_script.partition('DHCPDynList')
+        if not (approx_array.startswith(' = new Array(') and approx_array.endswith(');')):
+            raise RuntimeError('Failed to parse client list.')
+        array_content = approx_array[13:-3]
+        array_values = [x.strip('"') for x in array_content.split(', ')][:-1]
+        output = []
+        keys = ['name', 'mac', 'ip', 'lease_time']
+        curr = None
+        for i in range(len(array_values)):
+            if i % 4 == 0:
+                curr = {}
+                output.append(curr)
+            curr[keys[i%4]] = array_values[i]
+        return output
 
 
 if __name__ == '__main__':
-    print(TPLink().get_client_ips())
+    print(json.dumps(TPLink().get_client_ips(), indent=2))
